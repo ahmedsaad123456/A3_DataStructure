2c3251db9caf3a7b78e81e308a8dc69656d711aa
shahd osama <102310738+shahdosama10@users.noreply.github.com>
2023-05-08 18:40:06 +0300

    Update MaxHeap.h

--- MaxHeap.h
+++ MaxHeap.h
@@ -4,117 +4,22 @@
 #include<vector>
 #endif
 
-
 class MaxHeap {
 
-    vector<Student> ReadFromFile(string filename ){
-        
-        fstream datafile;
-        string line;
-        vector<Student> vec;
-        int counter = 1;
-        Student s;
-        
-        datafile.open(filename, ios::in | ios::out);
-        getline(datafile, line);
-        getline(datafile, line);
-        
-        while (!datafile.eof()) {
-            if(counter == 1)
-                s.setID(stoi(line));
-            
-            else if (counter == 2 )
-                s.setName(line);
-            
-            else if (counter == 3)
-                s.setGPA(stod(line));
-            
-            else {
-                s.setDepartment(line);
-                counter = 0;
-                vec.push_back(s);
-            }
-
-            counter++;
-            getline(datafile, line);
-        }
-
-        datafile.close();
-        return vec;
-    }
-    
+    vector<Student> ReadFromFile(string filename );
     vector<Student> studentsVec;
     
 public:
 
-    MaxHeap(){
-        this->studentsVec = ReadFromFile("students.txt");
-    }
+    MaxHeap();
 
-    void Add( Student s){
+    void Add( Student s);
 
-        studentsVec.push_back(s);
-        int index = studentsVec.size() - 1;
+    void heapify(vector<Student>& vec, int n, int i);
+
+    vector<Student>& Sort(vector<Student>& stVec);
+
+    void print();
         
-        while(index > 0   &&    studentsVec[index].getGPA()  >  studentsVec[ (index / 2) - 1 ].getGPA()) {
         
-            swap(studentsVec[(index / 2) - 1] , studentsVec[index]);
-            index = (index / 2) - 1;
-        }
-
-        cout<<"The Student is added"<<'\n';
-    }
-
-
-    void heapify(vector<Student>& vec , int n , int i){
-        
-        int largest = i;
-        int l = 2 * i + 1;
-        int r = 2 * i + 2;
-
-        if(l < n && studentsVec[l].getGPA() > studentsVec[largest].getGPA()){
-            largest = l;
-        }
-        
-        if(r < n && studentsVec[r].getGPA() > studentsVec[largest].getGPA()){
-            largest = r;
-        }
-        
-        if(largest != i){
-            
-            swap(vec[i] , vec[largest]);
-            heapify(vec , n , largest);
-        }
-    }
-
-    
-    vector<Student>& Sort(vector<Student>& stVec){
-        
-        int n = stVec.size();
-        
-        for(int i = n / 2 - 1 ; i >= 0 ; i--){
-            heapify(stVec , n , i);
-        }
-
-        for(int i = n - 1 ; i >= 0 ; i--){
-            swap(stVec[0] , stVec[i]);
-            heapify(stVec , i , 0);
-        }
-
-        return studentsVec;
-    }
-
-
-    void print(){
-        cout<<"Print "+to_string(studentsVec.size())+" students"<<'\n';
-        
-        studentsVec = Sort(studentsVec);
-        
-        for(int i = studentsVec.size() -1 ; i >= 0 ; i--){
-            studentsVec[i].print();
-        }
-    }
-  
-
-  
 };
