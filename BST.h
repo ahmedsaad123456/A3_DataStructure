049078740606ee24380d3d7abd43e13410d34415
Salmaabdelaziz271 <93342442+Salmaabdelaziz271@users.noreply.github.com>
2023-05-08 15:11:22 +0300

    add  BST.h

--- BST.h
+++ BST.h
@@ -3,4 +3,35 @@
 #include "Students.cpp"
 #endif
 
+struct Node{
+    Student student;
+    Node* left;
+    Node* right;
+    Node(Student student){
+        this->student = student;
+        this->left =NULL;
+        this->right = NULL;
+    }
+};
+
+class BST{
+    Node* root;
+    int size;
+    void ReadFromFile(string filename);
+    Node* insertNode(Node*, Student);
+    Node* deleteNode(Node*, int);
+    Node* minNode(Node*);
+
+public:
+    BST();
+    Node* getRoot();
+    void addStudent(Student);
+    void removeStudent(int);
+    bool searchStudent(int);
+    void printAll(Node*);
+
+};
+
+
+
 
